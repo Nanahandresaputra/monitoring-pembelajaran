ef981a8718d60e027880164c37740440ec7db1da
nanahandresaputra <nanaggmu@gmail.com>
2024-07-13 19:29:39 +0700

    integrate vercel

--- backend/src/app/auth/controller.js
+++ backend/src/app/auth/controller.js
@@ -6,40 +6,39 @@ const errorCode = require("../../middleware/errorCode.js");
 
 const login = async (req, res, next) => {
   let payload = req.body;
-
   try {
-    let user = await db.query(`SELECT * FROM userAdmin WHERE userAdmin.username = '${payload.username}'`);
-
-    if (!user.rows.length || maskingPwd(payload.password) !== user.rows[0].password) {
-      throw new Error("9005"); // Invalid username or password
+    let user = await db.query(`select * from userAdmin where userAdmin.username ='${payload.username}'`);
+    if (!user.rows.length || maskingPwd(payload.password) != user.rows[0].password) {
+      throw new Error(9005);
     }
+    if (user.rows[0].token != null) {
+      throw new Error(9006);
+    } else {
+      let signed = jwt.sign(
+        { id: user.rows[0].id, admin: user.rows[0].nama },
+        config.secretKey
+        //    {
+        //   expiresIn: 900,
+        // }
+      );
+      await db.query(`update userAdmin set token = '${signed}' where userAdmin.username = '${payload.username}'`);
 
-    if (user.rows[0].token !== null) {
-      throw new Error("9006"); // User already has a token
+      return res.json(errorCode(1000, "token", signed));
     }
-
-    let signed = jwt.sign(
-      { id: user.rows[0].id, admin: user.rows[0].nama },
-      config.secretKey
-      // Optionally, you can set expiresIn for the token here
-      // { expiresIn: '1h' }
-    );
-
-    await db.query(`UPDATE userAdmin SET token = '${signed}' WHERE userAdmin.username = '${payload.username}'`);
-
-    return res.json(errorCode(1000, "token", signed)); // Successful login with token
   } catch (err) {
-    if (err.message === "9005") {
-      return res.json(errorCode(9005)); // Invalid username or password
+    if (err && err.name === "error") {
+      res.json(errorCode(9003));
     }
-
     if (err.message === "9006") {
-      return res.json(errorCode(9006)); // User already has a token
+      return res.json(errorCode(9006));
+    }
+    if (err.message === "9005") {
+      return res.json(errorCode(9005));
+    } else {
+      res.json(errorCode(9002));
     }
 
-    // Handle other errors
-    console.error("Error during login:", err);
-    return res.json(errorCode(9002)); // General server error
+    next(err);
   }
 };
 
